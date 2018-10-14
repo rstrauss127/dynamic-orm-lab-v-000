863cd222c79252303da64efd36db3c5574a5b9ea
Rebecca Strauss <rstrauss127@gmail.com>
2018-10-14 19:58:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,62 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    sql = "PRAGMA table_info('#{table_name}')"
+    table_info = DB[:conn].execute(sql)
+    col_names = []
+    table_info.each do |column|
+      col_names << column["name"]
+    end
+    col_names.compact
+  end
+
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
+
+  def initialize(options = {})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
+
+  def table_name_for_insert
+    self.class.table_name
+  end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if{|col| col == "id"}.join(", ")
+  end
+
+  def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
+  end
+
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM #{table_name} WHERE name = ?
+
+    SQL
+    DB[:conn].execute(sql, name)
+  end
+
+  def self.find_by(name: , grade:)
+
+    DB[:conn].execute("SELECT * FROM #{table_name} WHERE name = ? AND grade = ?", name, grade)
+  end
 end
